cf13dd3b8510d33538a5cc8ebfead2054c53fe6d
protelescristata <gian.silvan.hiltbrunner@gmail.com>
2018-11-10 12:43:11 +0100

    Updates

---
+++ precache-manifest.112ad381b7d606a605f49c43c3bdf0e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9270ff0df70284d46b2",
+    "url": "/protelescristata/colorseries/static/css/main.9c1ce0c2.chunk.css"
+  },
+  {
+    "revision": "a9270ff0df70284d46b2",
+    "url": "/protelescristata/colorseries/static/js/main.a9270ff0.chunk.js"
+  },
+  {
+    "revision": "aa763f4e07fe091a8dd1",
+    "url": "/protelescristata/colorseries/static/js/1.aa763f4e.chunk.js"
+  },
+  {
+    "revision": "db90f4a7ec6a1a006f96",
+    "url": "/protelescristata/colorseries/static/js/runtime~main.db90f4a7.js"
+  },
+  {
+    "revision": "d93b7d6f2fa8bb00bbf7f129538ed82f",
+    "url": "/protelescristata/colorseries/index.html"
+  }
+];
